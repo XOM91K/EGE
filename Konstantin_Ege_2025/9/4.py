037ed3ab981285776972b4c0e47dbf2561c05694
XOM91K <moohctp@gmail.com>
2024-05-19 10:35:53 +0300

    добавлены задачи

--- Konstantin_Ege_2025/9/4.py
+++ Konstantin_Ege_2025/9/4.py
@@ -1,5 +1,6 @@
 l = [sorted([int(d) for d in x.split()]) for x in open('4.txt')]
 ct = 0
+ct = 0
 for x in l:
     if len(set(x)) == 5:
         if (x[0] + x[5]) / 2 > (x[1] + x[2] + x[3] + x[4]) / 4:
