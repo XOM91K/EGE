94bb01068abdb53fcd7af5ee231e92d3a1e0a55b
XOM91K <moohctp@gmail.com>
2024-09-16 20:03:48 +0300

    добавлены задачи

--- Timur_Ege_2025/2/4.py
+++ Timur_Ege_2025/2/4.py
@@ -3,5 +3,5 @@ for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                if ((x == (w or y)) or ((w <= z) and (y <= w))) == 0:
+                if ((w or x or y) <= ((y or z) and x or y and (w or z))) == 0:
                     print(x, y, z, w)
