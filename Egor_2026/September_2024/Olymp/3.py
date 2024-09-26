47e941381875c4708e8aa7f82a709642ba781a9f
XOM91K <moohctp@gmail.com>
2024-09-26 19:02:45 +0300

    добавлены задачи

---
+++ Egor_2026/September_2024/Olymp/3.py
@@ -0,0 +1,20 @@
+# red blue
+# 1. rbb
+# 2. brr
+# 4.  rbb brr rbb
+# O(n**2)
+# O(n)
+# O(1)
+# rbb brr rbb brr rbb
+n = 990745713
+if n % 3 == 0:
+    print(n // 3 * 2)
+else:
+    print(n // 3 * 2 + 1)
+#3 2
+#4 3
+#5 3
+#6 4
+#7 5
+#8 5
+#9 6
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import random
-l = [rnd for x in range(10) if (rnd := random.randint(5, 100)) % 2 == 0]
-print(l)
+import functools
+@functools.lru_cache(None)
+def F(n):
+    if n > 2024:
+        return 2
+    if n == 2024:
+        return 1
+    if n < 2024:
+        return n * (n + 1) + F(n + 1) - F(n + 2)
+for x in range(2026, 5, -1):
+    F(x)
+    print(x)
+print(F(100) - F(10) + F(2020))
