6ea4e8c34da9a531bc003432f64b2b0ec987102f
XOM91K <moohctp@gmail.com>
2024-10-15 22:07:17 +0300

    добавлены задачи

--- Nikita_OOP_2025/6.py
+++ Nikita_OOP_2025/6.py
@@ -25,6 +25,9 @@ class Point:
     def __repr__(self):
         return f"Point('{self.name}', {self.x},{self.y})"
 
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
+
 class CheckMark(Point):
     def __init__(self, pt1, pt2, pt3):
         self.pt1 = pt1
@@ -36,6 +39,9 @@ class CheckMark(Point):
 
     def __repr__(self):
         return f'{self.pt1} {self.pt2} {self.pt3}'
+
+    def __eq__(self, other):
+        return (self.pt1, self.pt2, self.pt3) == (other.pt1, other.pt2, other.pt3) or (self.pt1, self.pt2, self.pt3) == (other.pt3, other.pt2, other.pt1)
 
     def __bool__(self):
         string = f'{str(self.pt1)} {str(self.pt2)} {str(self.pt3)}'
@@ -50,18 +56,30 @@ class CheckMark(Point):
         return True
 
 
-p_A = Point('A', 1, 2)
-p_B = Point('B', 0, 1)
-p_C = Point('C', -1, 2)
-p_D = Point('D', 2, 2)
-p_E = Point('E', 2, 0)
-p_F = Point('F', 2, -1)
-cm_ABC = CheckMark(p_A, p_B, p_C)
-cm_DEF = CheckMark(p_D, p_E, p_F)
-cm_ABB = CheckMark(p_A, p_B, p_B)
-print(cm_ABC, bool(cm_ABC))
-print(cm_DEF, bool(cm_DEF))
-print(cm_ABB, bool(cm_ABB))
+# p_A = Point('A', 1, 2)
+# p_B = Point('B', 0, 1)
+# p_C = Point('C', -1, 2)
+# p_D = Point('D', 2, 2)
+# p_E = Point('E', 2, 0)
+# p_F = Point('F', 2, -1)
+# cm_ABC = CheckMark(p_A, p_B, p_C)
+# cm_DEF = CheckMark(p_D, p_E, p_F)
+# cm_ABB = CheckMark(p_A, p_B, p_B)
+# print(cm_ABC, bool(cm_ABC))
+# print(cm_DEF, bool(cm_DEF))
+# print(cm_ABB, bool(cm_ABB))
+#
+# p_A = Point('A', 1, 2)
+# p_B = Point('B', 0, 1)
+# p_C = Point('C', -1, 2)
+# p_D = Point('D', -1, 2)
+# cm_ABC = CheckMark(p_A, p_B, p_C)
+# cm_CBA = CheckMark(p_C, p_B, p_A)
+# cm_ACB = CheckMark(p_A, p_C, p_B)
+# cm_ABD = CheckMark(p_A, p_B, p_D)
+# cm_DBA = CheckMark(p_D, p_B, p_A)
+# print(cm_ABC == cm_CBA, cm_ABC == cm_ABD)
+# print(cm_ABC == cm_DBA, cm_ABC == cm_ACB)
 
 # p_A1 = Point('A', 1, 2)
 # p_A2 = Point('A', 2, 1)
