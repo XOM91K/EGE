b277997f3d2998e5335959cce6c8ceb5c8c19cd4
XOM91K <moohctp@gmail.com>
2024-10-29 16:53:09 +0300

    добавлены задачи

---
+++ Nikita_OOP_2025/db_chinook2.py
@@ -0,0 +1,16 @@
+import sqlite3
+name = input()
+with sqlite3.connect('Chinook_Sqlite.sqlite') as con:
+    cur = con.cursor()
+    cur.execute(f"SELECT GenreId FROM Genre WHERE Name = '{name}'")
+    id_name = cur.fetchone()
+    cur.execute(f"SELECT TrackId FROM Track WHERE GenreId = {id_name[0]}")
+    albums = cur.fetchall()
+    name_Track_sets = []
+    for x in albums:
+        cur.execute(f"SELECT Name FROM Track WHERE GenreId = {x[0]}")
+        nameTrack = cur.fetchall()
+        nameTrack = [g[0] for g in nameTrack]
+        name_Track_sets.extend(nameTrack)
+    for x in name_Track_sets:
+        print(x)
