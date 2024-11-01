03ad1b90df8a26006fbceb61f2740fd0701fc784
XOM91K <moohctp@gmail.com>
2024-11-01 21:40:41 +0300

    добавлены задачи

--- Nikita_OOP_2025/db_chinook2.py
+++ Nikita_OOP_2025/db_chinook2.py
@@ -8,16 +8,23 @@ with sqlite3.connect('Chinook_Sqlite.sqlite') as con:
     albums = [int(x[0]) for x in list(set(cur.fetchall()))]
     cur.execute(f"SELECT ArtistId FROM Artist")
     artist_ids = [int(x[0]) for x in cur.fetchall()]
-    name_Track_sets = []
+    name_Track_sets = {}
     for x in albums:
         for y in artist_ids:
             cur.execute(f"SELECT AlbumId FROM Album WHERE ArtistId = {y} AND AlbumId = {x}")
             nameTrack = cur.fetchone()
-            name_Track_sets.append(nameTrack)
-    name_Track_sets = [x[0] for x in name_Track_sets if x is not None]
-    name_albums = []
+            if y not in name_Track_sets:
+                name_Track_sets[y] = []
+            name_Track_sets[y].append(nameTrack)
     for x in name_Track_sets:
-        cur.execute(f"SELECT Title FROM Album WHERE AlbumId = {x}")
-        name_albums.append(cur.fetchone()[0])
-    for x in name_albums:
-        print(x)
+        name_Track_sets[x] = [x[0] for x in name_Track_sets[x] if x is not None]
+    name_albums = []
+    for y in name_Track_sets:
+        for x in range(len(name_Track_sets[y])):
+            cur.execute(f"SELECT Title FROM Album WHERE AlbumId = {name_Track_sets[y][x]}")
+            name_Track_sets[y][x] = cur.fetchone()[0]
+    for x in name_Track_sets:
+        name_Track_sets[x] = sorted(name_Track_sets[x])
+        if len(name_Track_sets[x]) > 0:
+            for y in name_Track_sets[x]:
+                print(y)
