6d812a45aa541c25b0ef936cf120606c5be3600c
XOM91K <moohctp@gmail.com>
2024-11-08 21:03:52 +0300

    добавлены задачи

---
+++ Matvey_Ege_2025/16/4.py
@@ -0,0 +1,13 @@
+import sys
+sys.setrecursionlimit(100000)
+def F(n):
+    if n == 0:
+        return 0
+    if n % 2 != 0:
+        return F(n - 1) + 2 * n - 1
+    if n % 2 == 0:
+        return 4*F(n / 2)
+for a in range(1000):
+    for b in range(1000):
+        if F(a) - F(b) == 1045:
+            print(a - b)
