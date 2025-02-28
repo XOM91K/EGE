7e1912462b464e37d39c25e30751f6724c4a2d63
XOM91K <moohctp@gmail.com>
2025-02-28 19:09:11 +0300

    добавлены задачи

---
+++ Matvey_Ege_2025/8/12.py
@@ -0,0 +1,9 @@
+import itertools
+k = 0
+for x in itertools.product('ГАЛКТИ', repeat=8):
+    x = ''.join(x)
+    if x[0] in 'ГЛКТ' and x[-1] in 'АИ':
+        if 'КЛ' not in x:
+                k += 1
+
+print(k)
