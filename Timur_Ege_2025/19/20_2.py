374ceb176b2bb7607ea4d57e0f38404443b27ef0
XOM91K <moohctp@gmail.com>
2025-06-03 17:58:00 +0300

    добавлены задачи

---
+++ Timur_Ege_2025/19/20_2.py
@@ -0,0 +1,15 @@
+#https://examinf.ru/tasks/930
+def g(s, p):
+    if s >= 172 and p == 3:
+        return 1
+    if s < 172 and p == 3:
+        return 0
+    if s >= 172 and p < 3:
+        return 0
+    if p % 2 == 0:
+        return g(s + 1, p + 1) or g(s + 2, p + 1) or g(s + 3, p + 1) or g(s * 2, p + 1)
+    else:
+        return g(s + 1, p + 1) and g(s + 2, p + 1) and g(s + 3, p + 1) and g(s * 2, p + 1)
+for S in range(1, 172):
+    if g(S, 0):
+        print(S)
