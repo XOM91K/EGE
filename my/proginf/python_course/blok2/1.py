32933e2e4d9e05d7db85894dde5c1ad93ca88ec5
XOM91K <moohctp@gmail.com>
2025-08-15 11:10:45 +0300

    добавлены задачи

--- my/proginf/python_course/blok2/1.py
+++ my/proginf/python_course/blok2/1.py
@@ -1,10 +1,9 @@
-s = "Hello!"
-
-print(s.upper())
-# HELLO!
-
-print(s.lower())
-# hello!
-
-print(s)
-# Hello!
+string = input("Введите числа через пробел:")
+list_of_strings = string.split() # список строковых представлений чисел
+print(list_of_strings)
+# ['1', '2', '3', '4']
+list_of_numbers = list(map(int, list_of_strings)) # cписок чисел
+print(list_of_numbers)
+# [1, 2, 3, 4]
+print(sum(list_of_numbers[::2])) # sum() вычисляет сумму элементов списка
+# 5
