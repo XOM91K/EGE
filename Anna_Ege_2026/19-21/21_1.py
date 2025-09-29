c2c023675f3597d1c8f74f1f3c298e4b9d24e8c0
XOM91K <moohctp@gmail.com>
2025-09-29 19:26:00 +0300

    добавлены задачи

---
+++ Anna_Ege_2026/19-21/21_1.py
@@ -0,0 +1,14 @@
+def g(s, p):
+    if s >= 67 and (p == 2 or p == 4):
+        return 1
+    elif s < 67 and p == 4:
+        return 0
+    elif s >= 67 and p < 4:
+        return 0
+    if p % 2 == 0:
+        return g(s + 1, p + 1) and g(s + 3, p + 1) and g(s * 2, p + 1)
+    else:
+        return g(s + 1, p + 1) or g(s + 3, p + 1) or g(s * 2, p + 1)
+for S in range(1, 67):
+    if g(S, 0) == 1:
+        print(S)
---
+++ Anna_Ege_2026/19-21/21_3.py
@@ -0,0 +1,14 @@
+def g(s, p):
+    if s <= 73 and (p == 2 or p == 4):
+        return 1
+    elif s > 73 and p == 4:
+        return 0
+    elif s <= 73 and p < 4:
+        return 0
+    if p % 2 == 0:
+        return g(s - 2, p + 1) and g(s - 4, p + 1) and g(s // 2, p + 1)
+    else:
+        return g(s - 2, p + 1) or g(s - 4, p + 1) or g(s // 2, p + 1)
+for S in range(74, 1000):
+    if g(S, 0):
+        print(S)
