6178dab4d372ab2ea3f4e0ed5b16577fafdb0f38
XOM91K <moohctp@gmail.com>
2026-04-05 15:04:48 +0300

    добавлены задачи

---
+++ Kostya_Ege_2027/16/4.py
@@ -0,0 +1,19 @@
+import sys, functools
+sys.setrecursionlimit(100000)
+@functools.lru_cache(None)
+def F(n):
+    if n < 31054:
+        return F(n + 4) + 3020
+    if n >= 31054:
+        return 3 * (G(n - 2) - 15)
+@functools.lru_cache(None)
+def G(n):
+    if n >= 28:
+        return G(n -5) - 15
+    if n < 28:
+        return 3 * n - 4
+for x in range(1, 35000):
+    G(x)
+for x in range(35000, 1, -1):
+    F(x)
+print(F(15))
