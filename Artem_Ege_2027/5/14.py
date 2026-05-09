c24c3d31b0afa73eef9bebae8533f0555ce2935b
XOM91K <moohctp@gmail.com>
2026-05-09 15:53:24 +0300

    добавлены задачи

--- Artem_Ege_2027/5/14.py
+++ Artem_Ege_2027/5/14.py
@@ -1,4 +1,9 @@
 for N in range(4, 10000):
     R = bin(N)[2:]
     if N % 2 == 0:
-        R = R +
+        R = R + R[:3]
+    else:
+        R = '1' + R + '01'
+    R = int(R, 2)
+    if R > 600:
+        print(R)
